3e3c5e0f0e76cc86ed1930fce5649bd1aa0d6b59
Fintan Halpenny <fintan.halpenny@gmail.com>
2025-09-11 08:55:50 +0100

    git-storage: remove crate
    This crate is not being published and is not used anywhere, so it is
    being removed from the set of crates.

--- git-storage/t/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// Copyright © 2022 The Radicle Link Contributors
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-#[cfg(any(test, feature = "test"))]
-pub mod gen;
-#[cfg(test)]
-mod properties;
-#[cfg(any(test, feature = "test"))]
-pub mod tmp;
--- git-storage/t/src/properties.rs
+++
@@ -1,4 +0,0 @@
-// Copyright © 2022 The Radicle Link Contributors
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-mod odb;
